4d12f0de1e9dee0a0b87e90f83ed4ad7e3f70c0d
SepJa2.7 <jalayersepehr@gmail.com>
2020-02-10 00:50:51 -0500

    completing the Logger.java

--- src/Logger/Logger.java
+++ src/Logger/Logger.java
@@ -9,65 +9,60 @@ import java.util.Date;
 
 public class Logger {
     public static final int LOG_TYPE_SERVER = 1;
-    public static final int LOG_TYPE_CLIENT = 2;
+    public static final int LOG_TYPE_CLIENT = 0;
 
-    public void clientLog(String clientID, String action, String response) throws IOException {
-        String serverAcronym = clientID.substring(0, 3).toUpperCase();
-        final String dir = System.getProperty("user.dir");
-        String fileName = dir;
-        if (serverAcronym.equalsIgnoreCase("MTL")) {
-            fileName = dir + "\\src\\Logs\\Client\\MontrealUsers\\" + clientID + ".txt";
-        } else if (serverAcronym.equalsIgnoreCase("SHE")) {
-            fileName = dir + "\\src\\Logs\\Client\\SherbrookeUsers\\" + clientID + ".txt";
-        } else if (serverAcronym.equalsIgnoreCase("QUE")) {
-            fileName = dir + "\\src\\Logs\\Client\\QuebecUsers\\" + clientID + ".txt";
-        }
-
-        Date date = new Date();
-
-        String strDateFormat = "yyyy-MM-dd hh:mm:ss a";
-
-        DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
-
-        String formattedDate = dateFormat.format(date);
-
-
-        FileWriter fileWriter = new FileWriter(fileName, true);
+    public static void clientLog(String clientID, String action, String response) throws IOException {
+        FileWriter fileWriter = new FileWriter(getFileName(clientID, LOG_TYPE_CLIENT), true);
         PrintWriter printWriter = new PrintWriter(fileWriter);
-        printWriter.println("DATE: " + formattedDate + "Client Action: " + action + " | Server Response: " + response);
+        printWriter.println("DATE: " + getFormattedDate() + "Client Action: " + action + " | Server Response: " + response);
 
         printWriter.close();
-
     }
 
 
-    public void serverLog(String clientID, String requestType, String requestParams, String requestResult, String serverResponse) throws IOException {
-        String serverAcronym = clientID.substring(0, 3).toUpperCase();
+    public static void serverLog(String serverID, String clientID, String requestType, String requestParams, String requestResult, String serverResponse) throws IOException {
+
+        FileWriter fileWriter = new FileWriter(getFileName(serverID, LOG_TYPE_SERVER), true);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        printWriter.println("DATE: " + getFormattedDate() + "ClientID: " + clientID + " | RequestType: " + requestType + " | RequestParameters: " + requestParams + " | RequestResult: " + requestResult + " | ServerResponse: " + serverResponse);
+
+        printWriter.close();
+    }
+
+    public static void serverLog(String serverID, String msg) throws IOException {
+
+        FileWriter fileWriter = new FileWriter(getFileName(serverID, LOG_TYPE_SERVER), true);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        printWriter.println("DATE: " + getFormattedDate() + "Msg: " + msg);
+
+        printWriter.close();
+    }
+
+    private static String getFileName(String ID, int logType) {
         final String dir = System.getProperty("user.dir");
         String fileName = dir;
-        if (serverAcronym.equalsIgnoreCase("MTL")) {
-            fileName = dir + "\\src\\Log\\Server\\Montreal.txt";
-        } else if (serverAcronym.equalsIgnoreCase("SHE")) {
-            fileName = dir + "\\src\\Log\\Server\\Sherbrooke.txt";
-        } else if (serverAcronym.equalsIgnoreCase("QUE")) {
-            fileName = dir + "\\src\\Log\\Server\\Quebec.txt";
+        if (logType == LOG_TYPE_SERVER) {
+            if (ID.equalsIgnoreCase("MTL")) {
+                fileName = dir + "\\src\\Log\\Server\\Montreal.txt";
+            } else if (ID.equalsIgnoreCase("SHE")) {
+                fileName = dir + "\\src\\Log\\Server\\Sherbrooke.txt";
+            } else if (ID.equalsIgnoreCase("QUE")) {
+                fileName = dir + "\\src\\Log\\Server\\Quebec.txt";
+            }
+        } else {
+            fileName = dir + "\\src\\Logs\\Client\\" + ID + ".txt";
         }
+        return fileName;
+    }
 
+    private static String getFormattedDate() {
         Date date = new Date();
 
         String strDateFormat = "yyyy-MM-dd hh:mm:ss a";
 
         DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
 
-        String formattedDate = dateFormat.format(date);
-
-
-        FileWriter fileWriter = new FileWriter(fileName, true);
-        PrintWriter printWriter = new PrintWriter(fileWriter);
-        printWriter.println("DATE: " + formattedDate + "ClientID: " + clientID + " | RequestType: " + requestType + " | RequestParameters: " + requestParams + " | RequestResult: " + requestResult + " | ServerResponse: " + serverResponse);
-
-        printWriter.close();
-
+        return dateFormat.format(date);
     }
 
 }
